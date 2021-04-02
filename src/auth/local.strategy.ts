d8ad0600a59f3057429332d1595f6584b729d5b8
Aarão Melo <aarao.melo.lopes@gmail.com>
2021-04-02 18:26:42 -0300

    Implementing Passport local

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,20 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from '../users/dto/login-user.dto';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async validate(loginUserDto: LoginUserDto): Promise<any> {
+    const user = await this.authService.validateUser(loginUserDto);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
