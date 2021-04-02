438c674d0059efe458aaa3464ad6374ef5aa27d8
Aarão Melo <aarao.melo.lopes@gmail.com>
2021-04-02 20:07:37 -0300

    end strategy jwt authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Request, Get, Post, UseGuards, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard'
+
 import { AuthService } from './auth/auth.service';
 
 @Controller()
@@ -15,10 +16,14 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    // req.user._doc.id = req.user._doc._id;
-    // delete req.user._doc._id
-    // delete req.user._doc.__v
-    // return req.user._doc;
     return this.authService.login(req.user)
   }
+
+  // @UseGuards(JwtAuthGuard)
+  // @Get('users')
+  // getProfile(@Request() req) {
+  //   console.log('ddddd');
+    
+  //   return req.user;
+  // }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 
 @Controller('users')
 export class UsersController {
@@ -13,26 +13,25 @@ export class UsersController {
     return await (this.usersService.create(createUserDto));
   }
 
-  @Get()
-  async login(@Body() loginUserDto: LoginUserDto) {
-    return await (this.usersService.login(loginUserDto));
-  }
-
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
